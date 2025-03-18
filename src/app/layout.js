c35afda2c566a81b8481a925160d8f1c31bbd474
Aman Khan <amankhancompq2002@gmail.com>
2025-03-19 01:40:07 +0530

    add metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,19 +2,23 @@ import Footer from "@/components/Footer";
 import Navbar from "../components/Navbar";
 import "./styles/globals.css";
 
-
 export const metadata = {
-  title: "",
-  description: "",
+  title: "Montrose Pet Clinic - Beaumont, AB",
+  description: "Montrose Pet Clinic provides top-notch veterinary care for your beloved pets in Beaumont, AB. From routine check-ups to emergency services, our experienced team is dedicated to ensuring your pets receive the best care possible.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body >
-        <Navbar/>
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </head>
+      <body>
+        <Navbar />
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );

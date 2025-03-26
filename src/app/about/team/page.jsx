1b64af276679df9f106caaee53fe01e04992f28d
Aman Khan <amankhancompq2002@gmail.com>
2025-03-27 02:52:25 +0530

    add daisy cat

--- src/app/about/team/page.jsx
+++ src/app/about/team/page.jsx
@@ -45,10 +45,11 @@ export default function OurTeam() {
         "Rebecca is a certified VOA from NorQuest College and joined MPC in 2023. She specializes in animal handling and enjoys hiking with her senior dog or playing video games with her cat in her spare time.",
     },
     {
-        name: "Dr. Rajpreet Kour",
-        img: "https://images.unsplash.com/photo-1491485880348-85d48a9e5312?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        name: "Dr. Daisy",
+        role: "(Clinic Cat)",
+        img: "/daisy.jpg",
         description:
-          "Coming Soon",
+          "Bio Coming Soon",
       },
   ];
 
